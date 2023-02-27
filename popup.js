f54bcd1baac9ae1067b00f6b01e6ba7a94582d0b
Luidmilla <mila291197@mail.ru>
2023-02-27 23:19:21 +0300

    student.html

--- popup.js
+++ popup.js
@@ -1,12 +1,24 @@
 let loginButton = document.querySelector('.login-button');
 let loginPopup = document.querySelector('.login-popup');
+let closeButton = document.querySelector('.popup-close');
 
 loginButton.addEventListener('click',showPopup);
+closeButton.addEventListener('click',close);
 
 function showPopup(evt) {
     console.log(evt);
-    evt.preventDefault()
+    evt.preventDefault();
 
-    loginPopup.classList.toggle('show-popup')
+    loginPopup.classList.toggle('show-popup');
 
 };
+
+function close() {
+    loginPopup.classList.remove('show-popup')
+}
+
+loginPopup.addEventListener('click',(e) =>{
+    if (e.target.classList.contains('show-popup')){
+        close();
+    }
+})
